a70419b247a96c52507e87cf6b8c3388f487a676
Tzuriel <tzurielyahav@gmail.com>
2025-12-17 18:33:29 +0200

    gui

--- src/Ex2_GUI.java
+++ src/Ex2_GUI.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
  * Intro2CS_2026A
  * This class represents a Graphical User Interface (GUI) for Map2D.
@@ -23,6 +28,23 @@ public class Ex2_GUI
      */
     public static Map2D loadMap(String mapFileName)
     {
+        try
+        {
+            File myFile = new File(mapFileName);
+            Scanner myReader = new Scanner(myFile);
+            int i=0;
+            while (myReader.hasNextLine())
+            {
+                String data = myReader.nextLine();
+                System.out.println(i+") "+data);
+                i=i+1;
+            }
+            myReader.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
         Map2D ans = new Map(4);
 
         return ans;
@@ -35,15 +57,39 @@ public class Ex2_GUI
      */
     public static void saveMap(Map2D map, String mapFileName)
     {
-
+//        int[][] mapArr = map.getMap();
+//        try
+//        {
+//            FileWriter myWriter = new FileWriter(mapFileName);
+//            myWriter.write("Text file named: "+file+"\n");
+//            myWriter.write("Point "+p1.toString());
+//            myWriter.close();
+//        }
+//        catch(Exception e)
+//        {
+//            e.printStackTrace();
+//        }
 
     }
 
     public static void main(String[] a)
     {
-        String mapFile = "map.txt";
-        Map2D map = loadMap(mapFile);
-        drawMap(map);
+//        String mapFile = "map.txt";
+//        Map2D map = loadMap(mapFile);
+//        drawMap(map);
+//
+//        String str = "file_123.txt";
+//        String data = "This is a multi String file,\n"
+//                + "second line ...\n ";
+//        save(str, data);
+//        try {
+//            load(str);
+//        } catch (FileNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        System.out.println("OK");
+
     }
 
     /// ///////////// Private functions ///////////////
