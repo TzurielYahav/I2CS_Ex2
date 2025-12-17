971f9e5887b02ae7b4a0b8ca8bcc203158c513b1
Tzuriel <tzurielyahav@gmail.com>
2025-12-17 20:50:09 +0200

    PACMAN

--- src/Ex2_GUI.java
+++ src/Ex2_GUI.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -16,6 +17,13 @@ import java.util.Scanner;
 public class Ex2_GUI
 {
     private static Map2D map;
+    private static Color[] colors = {
+            StdDraw.WHITE,
+            StdDraw.BLACK,
+            StdDraw.RED,
+            StdDraw.YELLOW,
+            StdDraw.GREEN
+    };
 
     public static void drawMap()
     {
@@ -23,9 +31,9 @@ public class Ex2_GUI
 //        int samples = 16;
         StdDraw.setXscale(0,maxX);
         StdDraw.setYscale(0,maxY);
-//        StdDraw.clear();
+        StdDraw.clear();
 
-        StdDraw.setPenColor(StdDraw.GRAY);
+
         drawGrid();
 //        StdDraw.setPenRadius(0.005);
 //        StdDraw.setPenColor(StdDraw.BLACK);
@@ -111,6 +119,9 @@ public class Ex2_GUI
         {
             saveMap(map, mapFile);
         }
+        int[][] mapArr = new int[10][10];
+        mapArr[1][1] = 3;
+        map.init(mapArr);
         drawMap();
         saveMap(map, mapFile);
     }
@@ -119,6 +130,19 @@ public class Ex2_GUI
 
     public static void drawGrid()
     {
+
+//        StdDraw.setPenColor(StdDraw.BLACK);
+//        drawArea(po1,po2, xx[0], 10, samples);
+//        StdDraw.setPenColor(StdDraw.BLUE);
+//        drawPoly(po1, min, max,n);
+//        drawInfo(po2,0,8);
+//        StdDraw.setPenColor(StdDraw.GREEN);
+//        drawPoly(po2, min, max,n);
+//        drawInfo(po1,0,7);
+
+        // ==== Grid ====
+        StdDraw.setPenRadius(0.01);
+        StdDraw.setPenColor(StdDraw.GRAY);
         for (int i = 0; i < map.getHeight(); i++)
         {
             StdDraw.line(0,i,map.getWidth(),i);
@@ -126,6 +150,17 @@ public class Ex2_GUI
         for (int i = 0; i < map.getWidth(); i++)
         {
             StdDraw.line(i,0,i,map.getHeight());
+        }
+
+        // ==== Cells ====
+        //StdDraw.setPenRadius(1);
+        for (int y = 0; y < map.getHeight(); y++)
+        {
+            for (int x = 0; x < map.getWidth(); x++)
+            {
+                StdDraw.setPenColor(colors[map.getPixel(x, y)]);
+                StdDraw.filledCircle(x, y, 0.2);
+            }
         }
     }
 
